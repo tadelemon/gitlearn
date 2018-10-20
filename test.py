ed17251f3b1ec53bd4bdf3f7ae9a0635f7c52001
tade <331058938@qq.com>
2018-10-20 20:18:59 +0800

    1111111

---
+++ test.py
@@ -0,0 +1,19 @@
+# age = 19
+# # print age
+# # print "hello world"
+# def dayin():
+#     return ';)'
+
+# c = dayin()
+# print dayin()
+
+def triangle():
+    a = int(input('fist number '))
+    b = int(input('last number '))
+    c =a**b
+    d={'lisi':23,'liliu':45}
+    print d
+    # print(f'the result is {c}')
+
+
+triangle()
